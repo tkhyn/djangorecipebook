3211051a0d6de3a666a96e4b5cc9026a899d94ad
Thomas Khyn <thomas@ksytek.com>
2014-04-05 03:09:45 +1300

    Implemented manage.Recipe

--- djangorecipebook/recipes/manage.py
+++ djangorecipebook/recipes/manage.py
@@ -2,8 +2,27 @@
 Recipe generating a management script
 """
 
+import os
+import sys
+
 from base import BaseRecipe
+from zc.buildout import easy_install
 
 
 class Recipe(BaseRecipe):
-    pass
+
+    def install(self):
+        __, working_set = self.egg.working_set()
+        return easy_install.scripts(
+            [(self.name, __name__, 'main')],
+            working_set, sys.executable, self.bin_dir,
+            extra_paths=self.extra_paths,
+            arguments="'%s'" % self.settings,
+            initialization=self.init)
+
+
+def main(settings_module):
+    # called on script execution
+    from django.core import management
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    management.execute_from_command_line(sys.argv)
---
+++ tests/manage.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import mock
+
+from base import ScriptTests, RecipeTests, test_project, test_settings
+
+from djangorecipebook.recipes import manage
+
+
+class ManageScriptTests(ScriptTests):
+
+    @mock.patch('django.core.management.execute_from_command_line')
+    @mock.patch('os.environ.setdefault')
+    def test_script(self, mock_setdefault, mock_execute):
+        # The manage script is a replacement for the default manage.py
+        # script. It has all the same bells and whistles since all it
+        # does is call the normal Django stuff.
+        manage.main(test_settings)
+        self.assertEqual(mock_execute.call_args,
+                         ((sys.argv,), {}))
+        self.assertEqual(
+            mock_setdefault.call_args,
+            (('DJANGO_SETTINGS_MODULE', test_settings), {}))
+
+
+class ManageRecipeTests(RecipeTests):
+
+    recipe_class = manage.Recipe
+    recipe_name = 'manage'
+    recipe_options = {'recipe': 'djangorecipebook:manage'}
+
+    @mock.patch('zc.recipe.egg.egg.Scripts.working_set',
+                    return_value=(None, []))
+    def test_install(self, working_set):
+        # Default install of a management script, check that the call to
+        # djangorecipebook.manage.main is present
+        self.recipe.install()
+        manage = self.script_path('manage')
+        self.assertTrue(os.path.exists(manage))
+        self.assertTrue(("djangorecipebook.recipes.manage.main('%s')" % \
+                         test_settings) in self.script_cat(manage))
+
+    @mock.patch('zc.recipe.egg.egg.Scripts.working_set',
+                    return_value=(None, []))
+    def test_create_manage_script_projectdir(self, working_set):
+        # When a project dir is specified, it should be added to sys.path
+        self.init_recipe({'project-dir': test_project})
+        self.recipe.install()
+        self.assertTrue(os.path.join(self.buildout_dir, test_project) \
+                        in self.script_cat('manage'))
+
+    @mock.patch('zc.recipe.egg.egg.Scripts.working_set',
+                    return_value=(None, []))
+    def test_create_manage_script_with_initialization(self, working_set):
+        # When an init code is specified, it should be added to the script
+        self.init_recipe({'initialization': 'import os\nassert True'})
+        self.recipe.install()
+        self.assertTrue('import os\nassert True\n\nimport djangorecipebook'
+                        in self.script_cat('manage'))
