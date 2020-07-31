06ebc07402e1f685f874153a716979ca52764fa2
Thomas Khyn <thomas@ksytek.com>
2020-07-31 23:23:16 +1200

    chore(setup): remove python 2 classifier

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
     keywords=['django', 'buildout', 'recipe'],
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
